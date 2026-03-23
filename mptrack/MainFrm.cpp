sagamusix/openmpt

commit be6ebfc8796d7c699fe47a344a644aaca3448c03

[Ref] Playback Test: Move gzip compression/decompression out into the caller.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@21441 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/MainFrm.cpp
@@ -56,6 +56,11 @@
 #include "openmpt/sounddevice/SoundDeviceBuffer.hpp"
 #include "openmpt/sounddevice/SoundDeviceManager.hpp"
 
+#ifdef MPT_ENABLE_PLAYBACK_TEST_MENU
+#include "../unarchiver/ungzip.h"
+#include "../common/GzipWriter.h"
+#endif
+
 #include <HtmlHelp.h>
 #include <Dbt.h>  // device change messages
 
@@ -66,10 +71,6 @@ static constexpr uint32 TIMERID_GUI = 1;
 static constexpr uint32 TIMERID_NOTIFY = 2;
 
 static constexpr uint32 MPTTIMER_PERIOD = 100;
-
-#if defined(MPT_BUILD_DEBUG)
-#define MPT_ENABLE_PLAYBACK_TEST_MENU
-#endif
 
 /////////////////////////////////////////////////////////////////////////////
 // CMainFrame
@@ -3268,7 +3269,17 @@ void CMainFrame::OnCreateMixerDump()
 		auto playTest = sndFile->CreatePlaybackTest(PlaybackTestSettings{});
 		mpt::ofstream outFile(fileName + P_(".testdata.gz"), std::ios::binary | std::ios::trunc);
 		if(outFile)
-			playTest.Serialize(outFile, fileName.GetFilename().ToUnicode() + U_(".testdata"));
+		{
+			std::ostringstream outStream;
+			playTest.Serialize(outStream);
+			#ifdef MPT_WITH_ZLIB
+				std::string outData = std::move(outStream).str();
+				WriteGzip(outFile, outData, fileName.GetFilename().ToUnicode() + U_(".testdata"));
+			#else
+				// miniz doesn't have gzip convenience functions
+				outFile << std::move(outStream).str();
+			#endif
+		}
 	}
 }
 
@@ -3299,7 +3310,16 @@ void CMainFrame::OnVerifyMixerDump()
 			if(!modFile.IsValid())
 				throw std::runtime_error{"Cannot open module data file: " + modFileName.ToUTF8()};
 
-			PlaybackTest playTest{GetFileReader(testFile)};
+			FileReader testFileReader = GetFileReader(testFile);
+			CGzipArchive archive{testFileReader};
+			if(archive.IsArchive())
+			{
+				if(!archive.ExtractFile(0))
+					throw std::runtime_error{"Cannot extract test data file!"};
+				testFileReader = archive.GetOutputFile();
+			}
+
+			PlaybackTest playTest{testFileReader};
 			auto sndFile = std::make_unique<CSoundFile>();
 			sndFile->Create(GetFileReader(modFile));
 

common/BuildSettings.h
@@ -114,6 +114,10 @@
 
 #define MPT_ENABLE_UPDATE
 
+#if defined(MPT_BUILD_DEBUG)
+#define MPT_ENABLE_PLAYBACK_TEST_MENU
+#endif
+
 // Disable unarchiving support
 //#define NO_ARCHIVE_SUPPORT
 

test/PlaybackTest.cpp
@@ -10,11 +10,9 @@
 
 #include "stdafx.h"
 #include "PlaybackTest.h"
-#include "../common/GzipWriter.h"
 #include "../soundlib/OPL.h"
 #include "../soundlib/SampleIO.h"
 #include "../soundlib/Sndfile.h"
-#include "../unarchiver/ungzip.h"
 
 #include "mpt/base/bit.hpp"
 #include "mpt/binary/hex.hpp"
@@ -331,13 +329,6 @@ PlaybackTest& PlaybackTest::operator=(PlaybackTest &&other) noexcept
 
 void PlaybackTest::Deserialize(FileReader file) noexcept(false)
 {
-	CGzipArchive archive{file};
-	if(archive.IsArchive())
-	{
-		if(!archive.ExtractFile(0))
-			throw std::runtime_error{"Cannot extract test data file!"};
-		file = archive.GetOutputFile();
-	}
 
 	file.Rewind();
 
@@ -377,26 +368,17 @@ void PlaybackTest::Deserialize(FileReader file) noexcept(false)
 }
 
 
-void PlaybackTest::Serialize(std::ostream &output, const mpt::ustring &filename) const noexcept(false)
+void PlaybackTest::Serialize(std::ostream &output) const noexcept(false)
 {
-	std::ostringstream outStream;
-	mpt::IO::Write(outStream, m_testData->header);
-	mpt::IO::Write(outStream, m_testData->sampleDataHashes);
+	mpt::IO::Write(output, m_testData->header);
+	mpt::IO::Write(output, m_testData->sampleDataHashes);
 	for(const auto &row : m_testData->rows)
 	{
-		mpt::IO::Write(outStream, row.header);
-		mpt::IO::Write(outStream, row.channels);
-		mpt::IO::WriteVarInt(outStream, row.oplRegisters.size());
-		mpt::IO::Write(outStream, row.oplRegisters);
+		mpt::IO::Write(output, row.header);
+		mpt::IO::Write(output, row.channels);
+		mpt::IO::WriteVarInt(output, row.oplRegisters.size());
+		mpt::IO::Write(output, row.oplRegisters);
 	}
-
-#ifdef MPT_WITH_ZLIB
-	std::string outData = std::move(outStream).str();
-	WriteGzip(output, outData, filename);
-#else
-	// miniz doesn't have gzip convenience functions
-	output << std::move(outStream).str();
-#endif
 }
 
 

test/PlaybackTest.h
@@ -34,7 +34,7 @@ public:
 	PlaybackTest& operator=(const PlaybackTest &) = delete;
 
 	void Deserialize(FileReader file) noexcept(false);
-	void Serialize(std::ostream &output, const mpt::ustring &filename) const noexcept(false);
+	void Serialize(std::ostream &output) const noexcept(false);
 	void ToTSV(std::ostream &output) const noexcept(false);
 
 	PlaybackTestSettings GetSettings() const noexcept;

commit 1be6de419f83661aa9296fd941b23b32e388feeb

[Fix] Converting compressed mixer dumps to TSV was broken since r21441.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@25082 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/MainFrm.cpp
@@ -3687,10 +3687,20 @@ void CMainFrame::OnConvertMixerDumpToText()
 		MPT_LOG_GLOBAL(LogDebug, "info", U_("Loading ") + fileName.ToUnicode());
 		try
 		{
-			mpt::IO::InputFile f(fileName);
-			if(!f.IsValid())
-				throw std::runtime_error{"Cannot open test data file!"};
-			PlaybackTest playTest{GetFileReader(f)};
+			mpt::IO::InputFile testFile{fileName};
+			if(!testFile.IsValid())
+				throw std::runtime_error{"Cannot open test data file: " + fileName.ToUTF8()};
+
+			FileReader testFileReader = GetFileReader(testFile);
+			CGzipArchive archive{testFileReader};
+			if(archive.IsArchive())
+			{
+				if(!archive.ExtractFile(0))
+					throw std::runtime_error{"Cannot extract test data file!"};
+				testFileReader = archive.GetOutputFile();
+			}
+
+			PlaybackTest playTest{testFileReader};
 			mpt::IO::ofstream output(fileName.ReplaceExtension(P_(".tsv")), std::ios::binary);
 			playTest.ToTSV(output);
 		} catch(const std::exception &e)