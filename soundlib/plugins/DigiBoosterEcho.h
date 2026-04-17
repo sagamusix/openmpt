sagamusix/openmpt

commit e9e2e07a62b36455284928ae66580039273b688f

[Ref] Plugins: DigiBooster: Add MPT_BINARY_STRUCT() check.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@25195 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/plugins/DigiBoosterEcho.h
@@ -118,4 +118,6 @@ protected:
 	void RecalculateEchoParams();
 };
 
+MPT_BINARY_STRUCT(DigiBoosterEcho::PluginChunk, 8);
+
 OPENMPT_NAMESPACE_END

commit 73cf999dc6162d01f29cfc10765d6b311f6c81ae

[Fix] Plugins: DigiBooster: Remove bogus semicolon. Broken since r25195.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@25197 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/plugins/DigiBoosterEcho.h
@@ -118,6 +118,6 @@ protected:
 	void RecalculateEchoParams();
 };
 
-MPT_BINARY_STRUCT(DigiBoosterEcho::PluginChunk, 8);
+MPT_BINARY_STRUCT(DigiBoosterEcho::PluginChunk, 8)
 
 OPENMPT_NAMESPACE_END